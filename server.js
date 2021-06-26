70a767f95f878366103ca425850af6dc1d471d4b
ALEXANDER <msumba83@gmail.com>
2021-06-26 15:48:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,39 @@
 
 // // handle request 
 
+const { response, request } = require("express");
 const express = require("express");
 const app = express();
 
 app.get("/", function (request, response) {
-  response.send("ALEXANDER IS LEARNING BACKEND WEB DEV at CYF");
+  response.send("Yay Node!");
 });
 
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
+app.get("/api", function (request, response) {
+  response.send("Hello World!");
+});
+
+app.get("/codeyourfuture", function (request, response) {
+  response.send('<h1>Alex is Learning Back-End Development</h1>');
+});
+
+app.get("/json", function (req, res) {
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
+});
+
+app.post('/api/json/codeyourfuture', (request, response) =>{
+    console.log('data', body);
+const body = request.body
+});
+
+app.get("/", function (req, res) {
+    co
+  let searchQuery = req.query.search;
+  res.send("Hello World! You searched for " + searchQuery);
 });

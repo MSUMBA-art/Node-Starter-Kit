a702565175aa6c177befc4cb1464085d701c3c10
ALEXANDER <msumba83@gmail.com>
2021-07-03 10:47:37 +0200

    added album obeject of arrays

--- server.js
+++ server.js
@@ -14,40 +14,81 @@
 
 // // handle request 
 
-const { response, request } = require("express");
-const express = require("express");
+// const { response, request } = require("express");
+// const express = require("express");
+// const app = express();
+
+// app.get("/", function (request, response) {
+//   response.send("Yay Node!");
+// });
+
+// app.listen(3000, function () {
+//   console.log("Server is listening on port 3000. Ready to accept requests!");
+// });
+
+// app.get("/api", function (request, response) {
+//   response.send("Hello World!");
+// });
+
+// app.get("/codeyourfuture", function (request, response) {
+//   response.send('<h1>Alex is Learning Back-End Development</h1>');
+// });
+
+// app.get("/json", function (req, res) {
+//   let lat = req.query.lat;
+//   let lng = req.query.lng;
+//   res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
+// });
+
+// app.post('/api/json/codeyourfuture', (request, response) =>{
+//     console.log('data', body);
+// const body = request.body
+// });
+
+
+// app.get("/", function (req, res) {
+//     co
+//   let searchQuery = req.query.search;
+//   res.send("Hello World! You searched for " + searchQuery);
+// });
+
+const express = require('express');
 const app = express();
 
-app.get("/", function (request, response) {
-  response.send("Yay Node!");
+app.get('/2', (req, res) => {
+  console.log('it is working');
+  res.send('ALEXANDER MSUMBA');
 });
 
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+const albumsData = [
+  {
+    albumId: "10",
+    artistName: "Beyoncé",
+    collectionName: "Lemonade",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+    releaseDate: "2016-04-25T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+  {
+    albumId: "12",
+    artistName: "Beyoncé",
+    collectionName: "Dangerously In Love",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+    releaseDate: "2003-06-24T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+];
+
+app.get("/albums", function (req, res) {
+  res.send(albumsData);
 });
 
-app.get("/api", function (request, response) {
-  response.send("Hello World!");
-});
-
-app.get("/codeyourfuture", function (request, response) {
-  response.send('<h1>Alex is Learning Back-End Development</h1>');
-});
-
-app.get("/json", function (req, res) {
-  let lat = req.query.lat;
-  let lng = req.query.lng;
-  res.send(`You searched for Lat: ${lat} and Lng: ${lng}`);
-});
-
-app.post('/api/json/codeyourfuture', (request, response) =>{
-    console.log('data', body);
-const body = request.body
-});
 
 
-app.get("/", function (req, res) {
-    co
-  let searchQuery = req.query.search;
-  res.send("Hello World! You searched for " + searchQuery);
-});
+app.listen(3000, () =>{
+  console.log('Server is listening on Port 3000');
+})
